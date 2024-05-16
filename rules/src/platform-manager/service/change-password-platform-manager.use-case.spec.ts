3a9e37385a15c45b84311cd756b5d5eed0b472d2
Diego <aldhiego666@gmail.com>
2024-05-16 15:09:24 -0300

    test: não pode atualizar gerente que não existe

--- rules/src/platform-manager/service/change-password-platform-manager.use-case.spec.ts
+++ rules/src/platform-manager/service/change-password-platform-manager.use-case.spec.ts
@@ -1,8 +1,11 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
 import { describe, expect, it } from 'vitest'
 import { CreatePlatformManager } from './create-platform-manager.use-case'
 import { InMemoryPlatformManagerRepository } from '../../mock/in-memory-platform-manager'
 import { BcryptPasswordService } from '../../mock/bcrypt-password-service'
 import { ChangePasswordPlatformManager } from './change-password-platform-manager.use-case'
+import { PlatformManager } from '../model/platform-manager.entities'
+import { AppError } from '../../shared/app-error'
 
 describe('Use case - Change password platform manager', () => {
   it('should be able to create a Platform manager', async () => {
@@ -26,16 +29,40 @@ describe('Use case - Change password platform manager', () => {
     )
 
     const newPassword = 'abcde'
-    const { platformManager: platformManagerWithNewPassword } =
+
+    await changePasswordPlatformManager.execute({
+      newPassword,
+      platformManagerId: platformManager.id,
+    })
+
+    const passwordHash = inMemoryRepository.platformManagers[0].props.password
+
+    const match = await bcryptPassword.compareHash(newPassword, passwordHash)
+    expect(match).toBeTruthy()
+  })
+
+  it('should not be able to update password for a manager that does not exist', async () => {
+    const inMemoryRepository = new InMemoryPlatformManagerRepository()
+    const bcryptPassword = new BcryptPasswordService()
+
+    const platformManager = new PlatformManager({
+      name: 'name example',
+      email: 'test@test.test',
+      password: '12345',
+    })
+
+    const changePasswordPlatformManager = new ChangePasswordPlatformManager(
+      inMemoryRepository,
+      bcryptPassword
+    )
+
+    const newPassword = 'abcde'
+
+    expect(async () => {
       await changePasswordPlatformManager.execute({
-        newPassword: 'abcde',
+        newPassword,
         platformManagerId: platformManager.id,
       })
-
-    const match = await bcryptPassword.compareHash(
-      newPassword,
-      platformManagerWithNewPassword.props.password
-    )
-    expect(match).toBeTruthy()
+    }).rejects.toThrowError(AppError)
   })
 })
