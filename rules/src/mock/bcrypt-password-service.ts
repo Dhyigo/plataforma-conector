497eb0ae299c3558fe4b41d1da2ebe12a915e6cc
Diego <aldhiego666@gmail.com>
2024-05-16 11:37:49 -0300

    feat: services fake to test

---
+++ rules/src/mock/bcrypt-password-service.ts
@@ -0,0 +1,13 @@
+import bcrypt from 'bcrypt'
+
+import { PasswordServices } from '../shared/interface/password-services'
+
+export class BcryptPasswordService implements PasswordServices {
+  async hash(password: string): Promise<string> {
+    return await bcrypt.hash(password, 8)
+  }
+
+  async compareHash(password: string, hash: string): Promise<boolean> {
+    return await bcrypt.compare(password, hash)
+  }
+}
---
+++ rules/src/mock/in-memory-platform-manager.ts
@@ -0,0 +1,28 @@
+import { PlatformManager } from '../platform-manager/model/platform-manager.entities'
+import { PlatformManagerRepository } from '../platform-manager/provider/platform-manager-repository'
+
+export class InMemoryPlatformManagerRepository
+  implements PlatformManagerRepository
+{
+  readonly platformManagers: PlatformManager[] = []
+
+  async create(platformManager: PlatformManager): Promise<void> {
+    this.platformManagers.push(platformManager)
+  }
+
+  async findById(platformManagerId: string): Promise<PlatformManager | null> {
+    const user = this.platformManagers.find((manager) => {
+      return manager.id === platformManagerId
+    })
+    return user ?? null
+  }
+
+  async update(platformManager: PlatformManager): Promise<void> {
+    const index = this.platformManagers.findIndex(
+      ({ id }) => id === platformManager.id
+    )
+    if (index === -1) {
+      throw new Error('PlatformManager not found')
+    }
+  }
+}
