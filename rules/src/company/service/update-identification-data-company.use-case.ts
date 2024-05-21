b861ae7bf940b5ad0fdc16013bb20117b783b284
Diego <aldhiego666@gmail.com>
2024-05-21 10:39:36 -0300

    feat: atualizar dados da empresa

---
+++ rules/src/company/service/update-identification-data-company.use-case.ts
@@ -0,0 +1,52 @@
+import { StatusCodes } from 'http-status-codes'
+import { AppError } from '../../shared/error/app-error'
+import { UseCase } from '../../shared/interface/use-case'
+import { Email } from '../../shared/object-value/email.object-value'
+import { SimpleName } from '../../shared/object-value/simple-name.value-object'
+import { Company } from '../model/company.entities'
+import { CompanyRepository } from '../provider/company-repository'
+import { Cnpj } from '../model/cnpj.value-object'
+
+interface UpdateIdentificationDataRequest {
+  companyId: string
+  newName: string
+  newEmail: string
+  newCnpj: string
+  newOrg: string
+}
+
+interface UpdateIdentificationDataResponse {
+  company: Company
+}
+
+export class UpdateIdentificationDataCompany implements UseCase {
+  constructor(private readonly repo: CompanyRepository) {}
+
+  async execute(
+    input: UpdateIdentificationDataRequest
+  ): Promise<UpdateIdentificationDataResponse> {
+    const { companyId, newName, newEmail, newCnpj, newOrg } = input
+
+    const company = await this.repo.findById(companyId)
+
+    if (company === null) {
+      throw new AppError({
+        message: 'Empresa não encontrada.',
+        statusCode: StatusCodes.NOT_FOUND,
+      })
+    }
+
+    company.changeIdentificationData({
+      name: new SimpleName(newName),
+      email: new Email(newEmail),
+      cnpj: new Cnpj(newCnpj),
+      org: new SimpleName(newOrg),
+    })
+
+    await this.repo.save(company)
+
+    return {
+      company,
+    }
+  }
+}
