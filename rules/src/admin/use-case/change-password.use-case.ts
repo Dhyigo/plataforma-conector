49d6b88510a62c55a090ce9c40fe3b0bb49b99e4
Diego <aldhiego666@gmail.com>
2024-05-28 15:42:08 -0300

    feat: trocar senha do admin

---
+++ rules/src/admin/use-case/change-password.use-case.ts
@@ -0,0 +1,45 @@
+import { StatusCodes } from 'http-status-codes'
+import { AppError } from '../../shared/error/app-error'
+import { PasswordServices } from '../../shared/interface/password-services'
+import { UseCase } from '../../shared/interface/use-case'
+import { Admin } from '../model/admin.entities'
+import { AdminRepository } from '../provider/admin-repository'
+
+interface ChangePasswordAdminRequest {
+  adminId: string
+  newPassword: string
+}
+
+interface ChangePasswordAdminResponse {
+  admin: Admin
+}
+
+export class ChangePasswordAdmin implements UseCase {
+  constructor(
+    private readonly repo: AdminRepository,
+    private readonly passwordService: PasswordServices
+  ) {}
+
+  async execute(
+    input: ChangePasswordAdminRequest
+  ): Promise<ChangePasswordAdminResponse> {
+    const { newPassword, adminId } = input
+    const admin = await this.repo.findById(adminId)
+
+    if (admin == null) {
+      throw new AppError({
+        message: 'Admin não encontrado.',
+        statusCode: StatusCodes.NOT_FOUND,
+      })
+    }
+
+    const password = await this.passwordService.hash(newPassword)
+    admin.changePassword(password)
+
+    await this.repo.save(admin)
+
+    return {
+      admin,
+    }
+  }
+}
