efe61757baa55d830697dd22d7c3308bcd1ad68a
Diego <aldhiego666@gmail.com>
2024-06-24 14:39:27 -0300

    ref: remove menu bar

--- external/frontend/src/component/menu-bar/index.tsx
+++
@@ -1,5 +0,0 @@
-import { MenubarRoot } from './menu-bar-root'
-
-export const Menubar = {
-  Root: MenubarRoot,
-}
--- external/frontend/src/component/menu-bar/menu-bar-root.tsx
+++
@@ -1,3 +0,0 @@
-export function MenubarRoot() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
-}
